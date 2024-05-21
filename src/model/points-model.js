80934fdfac22760feb60fb3460238d8e87408df5
KatriJein <Katrijein@yandex.ru>
2024-05-21 18:33:18 +0500

    7.10. Меняй-удаляй

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,11 +1,13 @@
+import Observable from '../framework/observable';
 import { getRandomPoint } from '../mock/point';
 
 const POINTS_COUNT = 12;
 
-export default class PointsModel {
+export default class PointsModel extends Observable {
   #points = null;
 
   constructor() {
+    super();
     this.#points = Array.from({ length: POINTS_COUNT }, getRandomPoint);
   }
 
@@ -13,13 +15,40 @@ export default class PointsModel {
     return this.#points;
   }
 
-  // #getMergedPoints(points) {
-  //   return points.map((point) => {
-  //     const offersByType = this.#offersModel.getOffersByType(point.type);
-  //     return {...point,
-  //       destination: this.#destinationModel.getDestinationById(point.destination),
-  //       offers: point.offers.map((offerId) => (offersByType.find((offer) => (offer.id === offerId)))),
-  //       offersByThisType: this.#offersModel.getOffersByType(point.type)};
-  //   });
-  // }
+  updatePoint(updateType, update) {
+    const index = this.#points.findIndex((point) => point.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting point');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, index),
+      update,
+      ...this.#points.slice(index + 1),
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  addPoint(updateType, update) {
+    this.#points = [
+      ...this.#points,
+      update
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  deletePoint(updateType, update) {
+    const index = this.#points.findIndex((point) => point.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting point');
+    }
+
+    this.#points = this.#points.filter((point) => point.id !== update.id);
+
+    this._notify(updateType);
+  }
 }
